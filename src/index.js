e8b613f547be878c9307ad5f15d1fe604947cdb5
AllanCh <edochacon96@gmail.com>
2019-09-26 11:00:45 -0600

    Corregir error  de login

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-
-//Configuracion y levantamiento del servidor
+//configuracion y levantamiento del servidor
 require("dotenv").config();
 require("./database");
-app.use(require("./routes/users"));
-app.use(require("./routes/user-device"));
+app.set("port", process.env.PORT || 4000);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.set("port", process.env.PORT || 4000);
+app.use(require("./routes/users"));
+app.use(require("./routes/user-device"));
 app.listen(app.get("port"), () => {
   console.log("Server on port", app.get("port"));
 });
